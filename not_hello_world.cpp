ce97d3710945d74a2f14279cdae9a176312658e1
Ezhh <owlecho@live.com>
2014-03-23 11:28:26 +0000

    Added namespace

--- not_hello_world.cpp
+++ not_hello_world.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 	{
-	std::cout << "I don't believe in Hello World. So I won't hello it. Sorry.";
+	cout << "I don't believe in Hello World. So I won't hello it. Sorry.";
 	return 0;
 	}
