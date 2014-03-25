0303c558c05408e5ba46dc8a906a2ef80bf2b019
Ezhh <owlecho@live.com>
2014-03-25 10:49:33 +0000

    Initial commit

---
+++ counting_me.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int x = 1;
+
+	if (x == 1)
+	{
+		cout "x silly Ezhh.";
+	}
+	else
+	{
+		while (x < 11)
+		{
+			cout << "x silly Ezhhes."
+		}
+	}
+return 0;
+}
