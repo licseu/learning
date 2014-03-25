b0c9c6d57946dff1b38110470fc6b0687d6cdb4a
Ezhh <owlecho@live.com>
2014-03-25 11:11:17 +0000

    Second commit

--- counting_me.cpp
+++ counting_me.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main ()
 {
-	int x = 1;
+        int x = 1;
 
-	if (x == 1)
-	{
-		cout "x silly Ezhh.";
-	}
-	else
-	{
-		while (x < 11)
-		{
-			cout << "x silly Ezhhes."
-		}
-	}
-return 0;
+        if (x == 1)
+        {
+                cout << x << " silly Ezhh.\n";
+                x++;
+        }
+
+        while (x < 11)
+        {
+                cout << x << " silly Ezhhes.\n";
+                x++;
+        }
+
+        return 0;
 }
