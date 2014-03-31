51bce7dead9cb310adeb613836d6660815c8d720
Ezhh <owlecho@live.com>
2014-03-31 08:40:28 +0100

    Just a start

---
+++ hangman.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+
+using namespace std; 
+
+int main ()
+{
+	char word;
+
+}
+
+
+//just committing this so we can get started. 
+//part I am working on will be in main for now as it will be the first thing needed.
+//feel free to add to this & add includes you need.
