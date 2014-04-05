1375459f797cf7c77a12fd95148e247863b68d38
Ezhh <owlecho@live.com>
2014-04-05 13:11:53 +0100

    tested array return

--- random.cpp
+++ random.cpp
@@ -7,14 +7,19 @@ using namespace std;
 
 int main ()
 {
-	srand (time(NULL));
-	int anumber = rand() % 10 + 1;
-
 	string words[10] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
-	string word = words[anumber];
+	//array test
+	int x = 0;
+    while (x<10)
+    {cout << words[x] << "\n"; x++;}
 
-	cout << "The word is " << word;
+
+	//returning random element from array
+	srand (time(NULL));
+	int anumber = rand() % 10 + 1;
+	string word = words[anumber];
+	cout << "\nA random word from the above is " << word << ".";
 	getchar();
 	return 0;
 }
