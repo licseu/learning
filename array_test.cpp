c52ad92ee356a674e7bf25984cb08cb377e3e6f2
Ezhh <owlecho@live.com>
2014-04-12 00:08:52 +0100

    simple example

---
+++ array_test.cpp
@@ -0,0 +1,27 @@
+
+//just a little test
+
+using namespace std;
+
+int main ()
+{
+	int test [5] [3] =
+	{
+		0,1,2,
+		3,4,5,
+		6,7,8,
+		9,10,11,
+		12,13,14,
+	};
+
+	cout << test [1] [2];
+	getchar();
+	return 0;
+
+}
+
+
+
+
+
+
