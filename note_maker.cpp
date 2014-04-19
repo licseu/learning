b7b50acebf6fa7e87b38ae52c6e448f07e67330a
Ezhh <owlecho@live.com>
2014-04-19 13:17:15 +0100

    restored 'close'

--- note_maker.cpp
+++ note_maker.cpp
@@ -1,11 +1,9 @@
-
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <ctime>
 
-using namespace std;
+using namespace std; 
 
 int main ()
 {
@@ -14,50 +12,42 @@ int main ()
 	ofstream thing;
 	cout << "Type a note to save in the text file:\n";
 	getline (cin, x);
-
+	
 	//time part
 	time_t now = time(0);
 	tm *ltm = localtime(&now);
 
 	//write to file part
 	thing.open ("stuff.txt", ios::app);
-
 	if (ltm->tm_mday < 10)
 	{
 		thing << "0";
 	}
-
 	thing << ltm->tm_mday << "/";
 	if (1 + ltm->tm_mon < 10)
 	{
 		thing << "0";
 	}
-
     thing << 1 + ltm->tm_mon << "/" << 1900 + ltm->tm_year << " - ";
-
  	if (1 + ltm->tm_hour < 10)
 	{
 		thing << "0";
-	}
-
+	}   
 	thing << 1 + ltm->tm_hour << ":";
-
 	if (1 + ltm->tm_min < 10)
 	{
 		thing << "0";
 	}
-
     thing << 1 + ltm->tm_min << ":";
-
 	if (1 + ltm->tm_sec < 10)
 	{
 		thing << "0";
 	}
-
     thing << 1 + ltm->tm_sec << "\n";
 	thing << "---------------------\n";
 	thing << x << "\n\n\n";
-
+	thing.close(); 
+	
 	cout << "See stuff.txt to view your note!\n" << endl;
 	getchar();
 	return 0;
